f800a33e226e878386edecf5c2b96e45cc11eec2
jeronimo20111021 <rockyboxeador25@gmail.com>
2026-04-01 00:02:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,25 +6,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Configura tu API Key de MailerSend desde Railway
 const mailerSend = new MailerSend({
   apiKey: process.env.MAILERSEND_API_KEY
 });
 
-// Ruta raíz para probar que el backend está vivo
 app.get("/", (req, res) => {
   res.send("Backend de RunnerBag funcionando ✅ con MailerSend");
 });
 
-// Endpoint de registro
 app.post("/predict", async (req, res) => {
   const { nombre, correo } = req.body;
 
   try {
-    // IMPORTANTE: usa tu Gmail verificado en MailerSend
     const sentFrom = new Sender("rockyboxeador25@gmail.com", "RunnerBag");
-
-    // Destinatario: tu propio Gmail para recibir notificaciones
     const recipients = [new Recipient("rockyboxeador25@gmail.com", "Rocky")];
 
     const emailParams = new EmailParams()
@@ -42,7 +36,6 @@ app.post("/predict", async (req, res) => {
   }
 });
 
-// Puerto Railway
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
